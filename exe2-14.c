0cf465e53b8179391e8309fdee0888c62a42222b
okakakai <kai48586@gmail.com>
2021-11-25 12:16:39 +0900

    exe2-14 commit

--- exe2-14.c
+++ exe2-14.c
@@ -9,17 +9,15 @@ int main()
                               "Friday",
                               "Saturday",
                               "Sunday"};
-
     printf("Please enter a number from 0-6\n");
     printf("0-月曜日 1-火曜日 2-水曜日 3-木曜日 4-金曜日 5-土曜日 6-日曜日\n");
+    fflush(stdout);
 
     int select = 0;
-    while (1)
-    {
-        fflush(stdin);
-        scanf("%d", &select);
-        printf("%s", WeekOfDay[select]);
-    }
+
+    fflush(stdin);
+    scanf("%d", &select);
+    printf("%s", WeekOfDay[select]);
 
     return 0;
 }
