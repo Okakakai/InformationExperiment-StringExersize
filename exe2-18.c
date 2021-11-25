3706dce6e9ebb171875e903758f26d2f254563a5
okakakai <kai48586@gmail.com>
2021-11-25 13:45:08 +0900

    exe2-18 commit

---
+++ exe2-18.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count = 0;
+    scanf("%s", str);
+    int i = 0;
+
+    while (1)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            count++;
+        }
+        else if (str[i] == '\0')
+        {
+            break;
+        }
+        i++;
+    }
+    if (count == 0)
+    {
+        printf("Not included");
+    }
+    else
+    {
+        printf("included : %ds", count);
+    }
+
+    return 0;
+}
